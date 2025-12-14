944321ffb01621eedd76f604290510a2227cd16f
The Sojourner <xerichos@gmail.com>
2025-12-14 05:54:33 -0300

    Fix the row insertion, deletion

--- gui/document.py
+++ gui/document.py
@@ -19,3 +19,43 @@ class TwoDADocument(QWidget):
         layout = QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.table)
+
+        # Connect context menu signals
+        self.table.requestInsertAbove.connect(self.insert_row_above)
+        self.table.requestInsertBelow.connect(self.insert_row_below)
+        self.table.requestDuplicate.connect(self.duplicate_row)
+        self.table.requestDelete.connect(self.delete_row)
+
+    def insert_row_above(self):
+        row = self.table.currentRow()
+        if row < 0:
+            return
+        if self.model.insertRows(row, 1):
+            self._mark_dirty()
+
+    def insert_row_below(self):
+        row = self.table.currentRow()
+        if row < 0:
+            return
+        if self.model.insertRows(row + 1, 1):
+            self._mark_dirty()
+
+    def duplicate_row(self):
+        row = self.table.currentRow()
+        if row < 0:
+            return
+        if self.model.duplicateRow(row):
+            self._mark_dirty()
+
+    def delete_row(self):
+        row = self.table.currentRow()
+        if row < 0:
+            return
+        if self.model.removeRows(row, 1):
+            self._mark_dirty()
+
+    def _mark_dirty(self):
+        self.is_dirty = True
+        mw = self.window()
+        if mw and hasattr(mw, "update_tab_title"):
+            mw.update_tab_title(self)
